c5ee8830b95ee4008c134aed17e95b418fedd853
Felipe Moraes <felipemoraes@Felipes-MacBook-Air.local>
2015-04-16 20:52:55 -0300

    Finished boolean search.

--- src/searcher/main.cc
+++ src/searcher/main.cc
@@ -10,10 +10,77 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+
+#include <sys/time.h>
+#include <sys/resource.h>
 #include "../../lib/index/index_file.h"
 #include "../../lib/common/vocabulary.h"
 
 using namespace std;
+
+static vector<unsigned> intersection_terms(vector<Term>* &terms){
+    vector<unsigned> result;
+    vector<unsigned> v1;
+    vector<unsigned> v2;
+    result.clear();
+    v1.clear();
+    v2.clear();
+    if (terms->size() == 0) {
+        cout << "Empty result. Try other query." << endl;
+        return result;
+    }
+    Term term = terms->back();
+    for (auto it = term.docs_->begin(); it != term.docs_->end(); it++) {
+        v1.push_back(it->doc_id_);
+    }
+    terms->pop_back();
+    
+    for (auto it = terms->begin(); it!=terms->end(); it++) {
+        result.clear();
+        for (auto iter = it->docs_->begin(); iter != it->docs_->end(); iter++) {
+            v2.push_back(iter->doc_id_);
+        }
+        set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(), back_inserter(result));
+        v1 = result;
+        v2.clear();
+    }
+    sort(result.begin(),result.end());
+    auto aux = unique(result.begin(), result.end());
+    result.erase(aux, result.end());
+    return result;
+}
+
+static vector<unsigned> union_terms(vector<Term>* &terms){
+    vector<unsigned> result;
+    vector<unsigned> v1;
+    vector<unsigned> v2;
+    result.clear();
+    v1.clear();
+    v2.clear();
+    if (terms->size() == 0) {
+        cout << "Empty result. Try other query." << endl;
+        return result;
+    }
+    Term term = terms->back();
+    for (auto it = term.docs_->begin(); it != term.docs_->end(); it++) {
+        v1.push_back(it->doc_id_);
+    }
+    terms->pop_back();
+    
+    for (auto it = terms->begin(); it!=terms->end(); it++) {
+        result.clear();
+        for (auto iter = it->docs_->begin(); iter != it->docs_->end(); iter++) {
+            v2.push_back(iter->doc_id_);
+        }
+        set_union(v1.begin(), v1.end(), v2.begin(), v2.end(), back_inserter(result));
+        v1 = result;
+        v2.clear();
+    }
+    sort(result.begin(),result.end());
+    auto aux = unique(result.begin(), result.end());
+    result.erase(aux, result.end());
+    return result;
+}
 
 
 int main(int argc, char** argv){
@@ -28,77 +95,48 @@ int main(int argc, char** argv){
             input_directory = string(argv[i]);
         }
     }
-
+    
     IndexFile* index = new IndexFile( input_directory + "index");
     Vocabulary* vocabulary = new Vocabulary(input_directory + "vocabulary");
     
-
-    string word1,word2,op;
-    
-    while (true) {
-        cout << "Search something..." << endl;
-        cin >> word1 >> op >> word2;
-        int term_id1 = vocabulary->get_term_id(word1);
-        long seek1 = vocabulary->get_seek(term_id1);
-        int term_id2 = vocabulary->get_term_id(word2);
-        long seek2 = vocabulary->get_seek(term_id2);
-        Term term1 = index->read(seek1);
-        Term term2 = index->read(seek2);
-        vector<Doc>::iterator it;
-        if (term_id1 == -1 && term_id2 == -1) {
-            cout << "Nothing retrieved" << endl;
-        } else if (term_id1 == -1) {
-            for (it=term2.docs_->begin(); it!=term2.docs_->end(); it++) {
-                cout << it->doc_id_ << ' ';
+    string word, op;
+    int n,m;
+    cin>> n >> op;
+    for (int i = 0; i<n; i++) {
+        cin >> m;
+        vector<Term> *terms = new vector<Term>(m);
+        terms->clear();
+        /*variable for time measure*/
+        float execution_time = 0;
+        struct timeval t_start;
+        struct timeval t_end;
+        
+        gettimeofday(&t_start, NULL);
+        
+        for (int j = 0; j < m; j++) {
+            cin >> word;
+            int term_id = vocabulary->get_term_id(word);
+            long seek = vocabulary->get_seek(term_id);
+            Term term = index->read(seek);
+            if (term.term_id_ != -1) {
+                terms->push_back(term);
             }
-            cout << endl;
-        } else if (term_id2 == -1) {
-            for (it=term1.docs_->begin(); it!=term1.docs_->end(); it++) {
-                cout << it->doc_id_ << ' ';
-            }
-            cout << endl;
         }
+        vector<unsigned> result;
         if (!op.compare("and")){
-            vector<int> v1;
-            vector<int> v2;
-            for (it=term1.docs_->begin(); it!=term1.docs_->end(); it++) {
-                v1.push_back( it->doc_id_);
-            }
-   
-
-            for (it=term2.docs_->begin(); it!=term2.docs_->end(); it++) {
-                v2.push_back( it->doc_id_);
-            }
-            vector<int> result;
-            set_intersection(v1.begin(), v1.end(),
-                                  v2.begin(), v2.end(),
-                                  back_inserter(result));
-            
-            for (int n : result) {
-                cout << n << ' ';
-            }
-            cout << endl;
+            result = intersection_terms(terms);
         } else if (!op.compare("or")){
-            vector<int> v1;
-            vector<int> v2;
-            for (it=term1.docs_->begin(); it!=term1.docs_->end(); it++) {
-                v1.push_back( it->doc_id_);
-            }
-            
-            
-            for (it=term2.docs_->begin(); it!=term2.docs_->end(); it++) {
-                v2.push_back( it->doc_id_);
-            }
-            vector<int> result;
-            set_union(v1.begin(), v1.end(),
-                             v2.begin(), v2.end(),
-                             back_inserter(result));
-            
-            for (int n : result) {
-                cout << n << ' ';
-            }
-            cout << endl;
+            result = union_terms(terms);
         }
+       for (int n : result) {
+            cout << n << ' ';
+        }
+        cout << endl;
+        gettimeofday(&t_end, NULL);
+        execution_time = (t_end.tv_sec-t_start.tv_sec)*1000000;
+        execution_time = (execution_time+(t_end.tv_usec-t_start.tv_usec))/1000000;
+        printf("Execution time %.3f seconds\n", execution_time);
+        delete terms;
     }
     delete index;
     return EXIT_SUCCESS;
